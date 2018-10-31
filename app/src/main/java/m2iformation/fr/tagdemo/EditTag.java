30cffb56235a643377d98896891a711c2dd83c45
Chris <tof.giga@hotmail.fr>
2018-10-31 09:17:13 +0100

    TagDemo

--- app/src/main/java/m2iformation/fr/tagdemo/EditTag.java
+++ app/src/main/java/m2iformation/fr/tagdemo/EditTag.java
@@ -2,24 +2,50 @@ package m2iformation.fr.tagdemo;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class EditTag extends AppCompatImageView {
+public class EditTag extends AppCompatImageView implements View.OnTouchListener {
     private Color tagColor;
     private int tagWeight;
     private Bitmap fingerTag;
     private Color bakcgroundColor;
 
+    Paint paint = new Paint();
+    //Définir l'objet Paint
+    Path path = new Path();
+
     public EditTag(Context context) {
         super(context);
+        setOnTouchListener(this);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
     }
+
 
     //Ce constructeur est nécessaire car utilisé par l'inflater
     // (le programme qui construit l'écran)
     public EditTag(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnTouchListener(this);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+    }
+
+    // Cette méthode est exécutée chaque fois que Android redessine la zone.
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawPath(path, paint);
     }
 
     public void setBakcgroundColor(Color bakcgroundColor) {
@@ -54,15 +80,27 @@ public class EditTag extends AppCompatImageView {
         this.fingerTag = fingerTag;
     }
 
-    public void clear(){
+    public void clear() {
 
     }
 
-    public void saveJpg(String fichier){
+    public void saveJpg(String fichier) {
 
     }
 
-    public void savePng(String fichier){
+    public void savePng(String fichier) {
 
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {//Création d'une direction
+            path.moveTo(event.getX(), event.getY());
+        }
+        if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            path.lineTo(event.getX(), event.getY());
+        }
+        invalidate();
+        return true;
     }
 }
